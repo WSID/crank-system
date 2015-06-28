073b03934a4a5c499bec2d85e9bf683b36fc7031
WSID <jongsome@gmail.com>
2015-06-29 00:05:37 +0900

    crankveccommon: Adding missing header file.

---
+++ crankbase/crankveccommon.h
@@ -0,0 +1,44 @@
+#ifndef _CRANK_VEC_COMMON_HEADER
+#define _CRANK_VEC_COMMON_HEADER
+
+/* Copyright (C) 2015, WSID   */
+
+/* Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ 
+#ifndef _CRANKBASE_INSIDE
+#error "crankveccommon.h cannot be included directly."
+#endif
+
+typedef struct _CrankVecBool2 CrankVecBool2;
+typedef struct _CrankVecBool3 CrankVecBool3;
+typedef struct _CrankVecBool4 CrankVecBool4;
+typedef struct _CrankVecBoolN CrankVecBoolN;
+
+typedef struct _CrankVecInt2 CrankVecInt2;
+typedef struct _CrankVecInt3 CrankVecInt3;
+typedef struct _CrankVecInt4 CrankVecInt4;
+typedef struct _CrankVecIntN CrankVecIntN;
+
+typedef struct _CrankVecFloat2 CrankVecFloat2;
+typedef struct _CrankVecFloat3 CrankVecFloat3;
+typedef struct _CrankVecFloat4 CrankVecFloat4;
+typedef struct _CrankVecFloatN CrankVecFloatN;
+
+#endif
