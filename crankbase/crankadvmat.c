9f3735b8761fa9e81985597fac25b771672b194b
WSID <jongsome@gmail.com>
2015-08-17 08:54:22 +0900

    crankadvmat: Tide up codes.

--- crankbase/crankadvmat.c
+++ crankbase/crankadvmat.c
@@ -52,9 +52,7 @@
  * * Getting LU decomposition takes O(n cube) times.
  * * And getting product of diagonal components takes O(n) times.
  * This is much faster than O(n!) times.
- *
- *
- *
+ * 
  * Currently Crank System provides,
  * * LU Decomposition
  *   * A = L U
@@ -69,12 +67,6 @@
  *   * Gram Schmidt Process
  *   * Householder Method (Does not return Q)
  */
-
-
-static guint	float_hash (gconstpointer	v);
-static gboolean	float_equal (gconstpointer	v1, gconstpointer	v2);
-static gfloat*	float_dup (gfloat			f);
-static void		destroy_slice_float		(gpointer		ptr);
 
 
 
@@ -104,21 +96,15 @@ crank_lu_mat_float_n (	CrankMatFloatN*	a,
   	guint	k;
 
   	guint	n;
+  	
+  	g_return_val_if_fail (a != l, FALSE);
+  	g_return_val_if_fail (a != u, FALSE);
 
-  	if (! crank_mat_float_n_is_square (a)) {
-	  	crank_mat_float_n_fini (l);
-	  	crank_mat_float_n_fini (u);
-	  	g_warning ("Adv: MatFloatN: lu: non square: %u, %u", a->rn, a->cn);
-	  	return FALSE;
-	}
+	CRANK_MAT_WARN_IF_NON_SQUARE_RET ("Advmat-MatFloatN", "lu", a, FALSE);
 
   	n = a->rn;
 
-  	if (n == 0) {
-	  	crank_mat_float_n_fini (l);
-	  	crank_mat_float_n_fini (u);
-	  	return TRUE;
-	}
+  	if (n == 0) return TRUE;
 
   	if (n == 1) {
 	  	if (a->data[0] != 0) {
@@ -126,12 +112,8 @@ crank_lu_mat_float_n (	CrankMatFloatN*	a,
 			crank_mat_float_n_init (u, 1, 1, 1.0f);
 		  	return TRUE;
 		}
-		else {
-			// If a == [[0]] then a is singular and cannot be defactorized.
-		  	crank_mat_float_n_fini (l);
-		  	crank_mat_float_n_fini (u);
-		  	return FALSE;
-		}
+		// If a == [[0]] then a is singular and cannot be defactorized.
+		else return FALSE;
 	}
 
 	crank_mat_float_n_init_fill (l, n, n, 0);
@@ -203,15 +185,12 @@ crank_lu_p_mat_float_n (	CrankMatFloatN*		a,
 {
 	guint			i;
 	guint			j;
-	CrankMatFloatN	na = {0};
+	CrankMatFloatN	na;
+  	
+  	g_return_val_if_fail (a != l, FALSE);
+  	g_return_val_if_fail (a != u, FALSE);
 	
-
-  	if (! crank_mat_float_n_is_square (a)) {
-	  	crank_mat_float_n_fini (l);
-	  	crank_mat_float_n_fini (u);
-	  	g_warning ("Adv: MatFloatN: lu_p: non square: %u, %u", a->rn, a->cn);
-	  	return FALSE;
-	}
+	CRANK_MAT_WARN_IF_NON_SQUARE_RET ("Advmat-MatFloatN", "lu-pivot", a, FALSE);
 	
 	// Do pivoting.
 	// Maximaze diagnoal component.
@@ -261,62 +240,59 @@ crank_gram_schmidt_mat_float_n (	CrankMatFloatN*		a,
 	guint	j;
 	guint	k;
 	
-	if (a->rn == a->cn) {
-		e = g_new0 (CrankVecFloatN, a->rn);
-		crank_mat_float_n_init_fill (r, a->rn, a->rn, 0);
+	g_return_val_if_fail (a != q, FALSE);
+	g_return_val_if_fail (a != r, FALSE);
+	CRANK_MAT_WARN_IF_NON_SQUARE_RET ("Advmat-MatFloatN", "gram-schmidt", a, FALSE);
+	
+	e = g_new (CrankVecFloatN, a->rn);
+	crank_mat_float_n_init_fill (r, a->rn, a->rn, 0);
+	
+	for (i = 0; i < a->rn; i++) {
+		CrankVecFloatN	ac;
+		CrankVecFloatN	u;
 		
-		for (i = 0; i < a->rn; i++) {
-			CrankVecFloatN	ac;
-			CrankVecFloatN	u;
+		// u = a.col[i]
+		crank_mat_float_n_get_col (a, i, &ac);
+		crank_vec_float_n_copy (&ac, &u);
+	
+		// u -= proj(a.col[i], e[0..(i-1)])
+		// r = a.col[i] dot e
+		for (j = 0; j < i; j++) {
+			CrankVecFloatN	proj;
+			gfloat			dot;
+			dot = crank_vec_float_n_dot (e + j, &ac);
 			
-			// u = a.col[i]
-			crank_mat_float_n_get_col (a, i, &ac);
-			crank_vec_float_n_copy (&ac, &u);
-		
-			// u -= proj(a.col[i], e[0..(i-1)])
-			// r = a.col[i] dot e
-			for (j = 0; j < i; j++) {
-				CrankVecFloatN	proj;
-				gfloat			dot;
-				dot = crank_vec_float_n_dot (e + j, &ac);
-				
-				// If QR decomposition is not possible, return.
-				if (dot == 0.0f) {
-					for (k = 0; k < i; k++) crank_vec_float_n_fini (e + k);
-					g_free (e);
-					crank_vec_float_n_fini (&ac);
-					crank_vec_float_n_fini (&u);
-					crank_mat_float_n_fini (q);
-					crank_mat_float_n_fini (r);
-					return FALSE;
-				}
-				
-				crank_vec_float_n_muls (e + j, dot, &proj);
-				
-				crank_vec_float_n_sub_self (&u, &proj);
-				crank_mat_float_n_set (r, j, i, dot);
+			// If QR decomposition is not possible, return.
+			if (dot == 0.0f) {
+				for (k = 0; k < i; k++) crank_vec_float_n_fini (e + k);
+				g_free (e);
+				crank_vec_float_n_fini (&ac);
+				crank_vec_float_n_fini (&u);
+				crank_mat_float_n_fini (q);
+				crank_mat_float_n_fini (r);
+				return FALSE;
 			}
-		
-			// e[i] = u.unit
-			crank_mat_float_n_set (r, i, i, crank_vec_float_n_get_magn (&u));
-			crank_vec_float_n_unit (&u, e + i);
-			crank_vec_float_n_fini (&u);
+			
+			crank_vec_float_n_muls (e + j, dot, &proj);
+			
+			crank_vec_float_n_sub_self (&u, &proj);
+			crank_mat_float_n_set (r, j, i, dot);
 		}
-
-		crank_mat_float_n_init_cvarr (q, a->rn, e);
-
-		for (i = 0; i < a->rn; i++) {
-			crank_vec_float_n_fini (e + i);
-		}
-		g_free (e);
-		
-		return TRUE;
+	
+		// e[i] = u.unit
+		crank_mat_float_n_set (r, i, i, crank_vec_float_n_get_magn (&u));
+		crank_vec_float_n_unit (&u, e + i);
+		crank_vec_float_n_fini (&u);
 	}
-	else {
-		g_warning ("Adv: MatFloatN: gram schmidt: non square: %u, %u",
-				a->rn, a->cn);
-		return FALSE;
+
+	crank_mat_float_n_init_cvarr (q, a->rn, e);
+
+	for (i = 0; i < a->rn; i++) {
+		crank_vec_float_n_fini (e + i);
 	}
+	g_free (e);
+	
+	return TRUE;
 }
 
 /**
@@ -341,69 +317,64 @@ crank_qr_householder_mat_float_n (	CrankMatFloatN*	a,
 	CrankMatFloatN	qpai = {0};
 	CrankVecFloatN	an = {0};
 	
-	if (a->rn == a->cn) {
+	g_return_val_if_fail (a != r, FALSE);
+	CRANK_MAT_WARN_IF_NON_SQUARE_RET ("Advmat-MatFloatN", "gram-schmidt", a, FALSE);
 	
-		if (a->rn == 1) {
-			crank_mat_float_n_init (r, 0, 0,
-					crank_mat_float_n_get (a, 0, 0));
-			return TRUE;
-		}
-	
-	
-		// Setup initial state.
-		crank_mat_float_n_copy (a, &pa);
-		crank_mat_float_n_init_fill (r, a->rn, a->rn, 0.0f);
-		
-		for (i = 0; i < a->rn - 1; i++) {
-			// Initialize an
-			crank_mat_float_n_get_col (&pa, 0, &an);
-
-			crank_vec_float_n_set (&an, 0,
-					crank_vec_float_n_get (&an, 0) -
-					crank_vec_float_n_get_magn (&an));
-
-			crank_vec_float_n_unit_self (&an);
-			
-			// Initialize qi			
-			crank_mat_float_n_init_fill (&qi, a->rn - i, a->rn - i, 0.0f);
-			
-			for (j = 0; j < a->rn - i; j++) {
-				for (k = 0; k < a->rn - i; k++) {
-					crank_mat_float_n_set (&qi, j, k,
-							- 2 *
-							crank_vec_float_n_get (&an, j) *
-							crank_vec_float_n_get (&an, k));
-				}
-				
-				crank_mat_float_n_set (&qi, j, j,
-						1 + crank_mat_float_n_get (&qi, j, j));
-			}
-			
-			crank_mat_float_n_mul (&qi, &pa, &qpai);
-			
-			// Resulting row 0 of qpai is part of r
-			// and rest part is next pa
-			for (j = 0; j < a->rn - i; j++) {
-				crank_mat_float_n_set (r, i, i + j,
-						crank_mat_float_n_get (&qpai, 0, j));
-			}
-			crank_mat_float_n_slice (&qpai, 1, 1, qpai.rn, qpai.cn, &pa);
-		}
-		// Fill last part of r
-		crank_mat_float_n_set(r, (a->rn - 1), (a->rn - 1),
-				crank_mat_float_n_get (&qpai, 1, 1));
-		
-		crank_mat_float_n_fini (&pa);
-		crank_mat_float_n_fini (&qi);
-		crank_mat_float_n_fini (&qpai);
-		crank_vec_float_n_fini (&an);
+	if (a->rn == 1) {
+		crank_mat_float_n_init (r, 0, 0,
+				crank_mat_float_n_get (a, 0, 0));
 		return TRUE;
 	}
-	else {
-		g_warning ("Adv: MatFloatN: QR householder: unsupported size: not square: %u, %u",
-				a->rn, a->cn);
-		return FALSE;
+
+
+	// Setup initial state.
+	crank_mat_float_n_copy (a, &pa);
+	crank_mat_float_n_init_fill (r, a->rn, a->rn, 0.0f);
+	
+	for (i = 0; i < a->rn - 1; i++) {
+		// Initialize an
+		crank_mat_float_n_get_col (&pa, 0, &an);
+
+		crank_vec_float_n_set (&an, 0,
+				crank_vec_float_n_get (&an, 0) -
+				crank_vec_float_n_get_magn (&an));
+
+		crank_vec_float_n_unit_self (&an);
+		
+		// Initialize qi			
+		crank_mat_float_n_init_fill (&qi, a->rn - i, a->rn - i, 0.0f);
+		
+		for (j = 0; j < a->rn - i; j++) {
+			for (k = 0; k < a->rn - i; k++) {
+				crank_mat_float_n_set (&qi, j, k,
+						- 2 *
+						crank_vec_float_n_get (&an, j) *
+						crank_vec_float_n_get (&an, k));
+			}
+			
+			crank_mat_float_n_set (&qi, j, j,
+					1 + crank_mat_float_n_get (&qi, j, j));
+		}
+		
+		crank_mat_float_n_mul (&qi, &pa, &qpai);
+		
+		// Resulting row 0 of qpai is part of r
+		// and rest part is next pa
+		for (j = 0; j < a->rn - i; j++) {
+			crank_mat_float_n_set (r, i, i + j,
+					crank_mat_float_n_get (&qpai, 0, j));
+		}
+		crank_mat_float_n_slice (&qpai, 1, 1, qpai.rn, qpai.cn, &pa);
 	}
+	// Fill last part of r
+	crank_mat_float_n_set(r, (a->rn - 1), (a->rn - 1),
+			crank_mat_float_n_get (&qpai, 1, 1));
+	
+	crank_mat_float_n_fini (&pa);
+	crank_mat_float_n_fini (&qi);
+	crank_mat_float_n_fini (&qpai);
+	crank_vec_float_n_fini (&an);
+	return TRUE;
 }
 
 /**
@@ -425,69 +396,65 @@ crank_qr_givens_mat_float_n (	CrankMatFloatN*	a,
 	
 	CrankMatFloatN	pa = {0};
 	
-	if (a->rn == a->cn) {
 	
-		if (a->rn == 1) {
-			crank_mat_float_n_init (r, 0, 0,
-					crank_mat_float_n_get (a, 0, 0));
-			return TRUE;
-		}
-		
-		crank_mat_float_n_init_fill (r, a->rn, a->rn, 0.0f);
-		crank_mat_float_n_copy (a, &pa);
-		
-		for (i = 0; i < a->rn - 1; i++) {
-			guint	n = a->rn - i;
-			
-			for (j = n - 1; 0 < j; j--) {
-				CrankVecFloat2 x = {
-						crank_mat_float_n_get (&pa, j - 1, 0),
-						crank_mat_float_n_get (&pa, j, 0)};
-				
-				if ((x.x == 0) && (x.y == 0)) {
-					crank_mat_float_n_fini (&pa);
-					crank_mat_float_n_fini (r);
-					
-					return FALSE;
-				}
-				
-				crank_vec_float2_unit_self (&x);
-				
-				// Multiplies Givens rotation matrix.
-				//
-				// We don't build up Full givens rotation matrix,
-				// instead we apply this with sin, cos value.
-				for (k = 0; k < n; k++) {
-					gfloat	e = crank_mat_float_n_get (&pa, j - 1, k);
-					gfloat	f = crank_mat_float_n_get (&pa, j, k);
-					
-					crank_mat_float_n_set (&pa, j-1, k,
-							e * x.x + f * x.y);
-							
-					crank_mat_float_n_set (&pa, j, k,
-							- e * x.y + f * x.x);
-				}
-			}
-			
-			for (j = 0; j < n; j++) {
-				crank_mat_float_n_set (r, i, i + j,
-					crank_mat_float_n_get (&pa, 0, j));
-			}
-			
-			crank_mat_float_n_slice (&pa, 1, 1, pa.rn, pa.cn, &pa);
-		}
-		
-		crank_mat_float_n_set (r, a->rn - 1, a->rn - 1, pa.data[0]);
-		
-		crank_mat_float_n_fini (&pa);
-		
+	g_return_val_if_fail (a != r, FALSE);
+	CRANK_MAT_WARN_IF_NON_SQUARE_RET ("Advmat-MatFloatN", "gram-schmidt", a, FALSE);
+	
+	if (a->rn == 1) {
+		crank_mat_float_n_init (r, 1, 1,
+				crank_mat_float_n_get (a, 0, 0));
 		return TRUE;
 	}
-	else {
-		g_warning ("Adv: MatFloatN: QR householder: unsupported size: not square: %u, %u",
-				a->rn, a->cn);
-		return FALSE;
+	
+	crank_mat_float_n_init_fill (r, a->rn, a->rn, 0.0f);
+	crank_mat_float_n_copy (a, &pa);
+	
+	for (i = 0; i < a->rn - 1; i++) {
+		guint	n = a->rn - i;
+		
+		for (j = n - 1; 0 < j; j--) {
+			CrankVecFloat2 x = {
+					crank_mat_float_n_get (&pa, j - 1, 0),
+					crank_mat_float_n_get (&pa, j, 0)};
+			
+			if ((x.x == 0) && (x.y == 0)) {
+				crank_mat_float_n_fini (&pa);
+				crank_mat_float_n_fini (r);
+				
+				return FALSE;
+			}
+			
+			crank_vec_float2_unit_self (&x);
+			
+			// Multiplies Givens rotation matrix.
+			//
+			// We don't build up Full givens rotation matrix,
+			// instead we apply this with sin, cos value.
+			for (k = 0; k < n; k++) {
+				gfloat	e = crank_mat_float_n_get (&pa, j - 1, k);
+				gfloat	f = crank_mat_float_n_get (&pa, j, k);
+				
+				crank_mat_float_n_set (&pa, j-1, k,
+						e * x.x + f * x.y);
+						
+				crank_mat_float_n_set (&pa, j, k,
+						- e * x.y + f * x.x);
+			}
+		}
+		
+		for (j = 0; j < n; j++) {
+			crank_mat_float_n_set (r, i, i + j,
+				crank_mat_float_n_get (&pa, 0, j));
+		}
+		
+		crank_mat_float_n_slice (&pa, 1, 1, pa.rn, pa.cn, &pa);
 	}
+	
+	crank_mat_float_n_set (r, a->rn - 1, a->rn - 1, pa.data[0]);
+	
+	crank_mat_float_n_fini (&pa);
+	
+	return TRUE;
 }
 
 /**
@@ -512,38 +479,33 @@ crank_eval_power_mat_float_n (	CrankMatFloatN*	a,
 								CrankVecFloatN*	b,
 								CrankVecFloatN*	evec	)
 {
-	if (crank_mat_float_n_is_square (a)) {
-		CrankVecFloatN	bs;
-		CrankVecFloatN	bsmv;
-		gfloat			magn = 0;
-		gfloat			magnp = 0;
-		
-		if (b != NULL) {
-			crank_vec_float_n_copy (b, &bs);
-		}
-		else {
-			crank_mat_float_n_get_col (a, 0, &bs);
-		}
-		
-		do {
-			magnp = magn;
-			crank_mat_float_n_mulv (a, &bs, &bsmv);
-			magn = crank_vec_float_n_get_magn (&bsmv);
-			crank_vec_float_n_divs (&bsmv, magn, &bs);
-			
-			crank_vec_float_n_fini (&bsmv);
-		}
-		while (0.0001f < ABS (magn - magnp));
-		
-		if (evec != NULL) crank_vec_float_n_copy (&bs, evec);
-		crank_vec_float_n_fini (&bs);
-		return magn;
+	CrankVecFloatN	bs;
+	CrankVecFloatN	bsmv;
+	gfloat			magn = 0;
+	gfloat			magnp = 0;
+	
+	CRANK_MAT_WARN_IF_NON_SQUARE_RET ("Advmat-MatFloatN", "power-method", a, 0.0f);
+	
+	if (b != NULL) {
+		crank_vec_float_n_copy (b, &bs);
 	}
 	else {
-		g_warning ("Adv: MatFloatN: Power: not square: %u, %u",
-				a->rn, a->cn);
-		return 0.0f/0.0f;
+		crank_mat_float_n_get_col (a, 0, &bs);
 	}
+	
+	do {
+		magnp = magn;
+		crank_mat_float_n_mulv (a, &bs, &bsmv);
+		magn = crank_vec_float_n_get_magn (&bsmv);
+		crank_vec_float_n_divs (&bsmv, magn, &bs);
+		
+		crank_vec_float_n_fini (&bsmv);
+	}
+	while (0.0001f < ABS (magn - magnp));
+	
+	if (evec != NULL) crank_vec_float_n_copy (&bs, evec);
+	crank_vec_float_n_fini (&bs);
+	return magn;
 }
 
 /**
@@ -558,13 +520,15 @@ void
 crank_eval_qr_mat_float_n (	CrankMatFloatN*	a,
 							CrankVecFloatN*	evals	)
 {
-	CrankMatFloatN	ai = {0};
-	CrankMatFloatN	qi = {0};
-	CrankMatFloatN	ri = {0};
+	CrankMatFloatN	ai;
+	CrankMatFloatN	qi;
+	CrankMatFloatN	ri;
 	gboolean		cont;
 	
 	guint	i;
 	guint	j;
+	
+	CRANK_MAT_WARN_IF_NON_SQUARE ("Advmat-MatFloatN", "power-method", a);
 	
 	cont = TRUE;
 	
@@ -605,32 +569,4 @@ crank_eval_qr_mat_float_n (	CrankMatFloatN*	a,
 	crank_mat_float_n_fini (&ai);
 	crank_mat_float_n_fini (&qi);
 	crank_mat_float_n_fini (&ri);
-}
-
-
-
-static guint
-float_hash (gconstpointer	f) {
-	gdouble d = (gdouble) (*(gfloat*)f);
-	
-	return g_double_hash (&d);
-}
-
-static gboolean
-float_equal (gconstpointer	p1, gconstpointer	p2) {
-	gfloat f1 = *(gfloat*)p1;
-	gfloat f2 = *(gfloat*)p2;
-	return (p1 == p2);
-}
-
-static gfloat*
-float_dup (gfloat	f) {
-	gfloat*	result = g_slice_new (gfloat);
-	*result = f;
-	return result;
-}
-
-static void
-destroy_slice_float (gpointer	ptr) {
-	g_slice_free (gfloat, ptr);
 }
