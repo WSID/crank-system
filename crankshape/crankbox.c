d187dafa9c49ab4402e9f2eec724413c93f1735a
WSID <jongsome@gmail.com>
2015-11-13 19:38:22 +0900

    CrankBox3: Added initial implementation.

--- crankshape/crankbox.c
+++ crankshape/crankbox.c
@@ -41,6 +41,7 @@
 //////// Type Definitions //////////////////////////////////////////////////////
 
 G_DEFINE_BOXED_TYPE (CrankBox2, crank_box2, crank_box2_dup, g_free)
+G_DEFINE_BOXED_TYPE (CrankBox3, crank_box3, crank_box3_dup, g_free)
 
 //////// Public Functions //////////////////////////////////////////////////////
 
@@ -367,6 +368,381 @@ crank_box2_get_intersection (CrankBox2 *box,
 
   if (intersect != NULL)
     crank_box2_init_range (intersect, &ix, &iy);
+
+  return result;
+}
+
+
+
+
+
+
+/**
+ * crank_box3_init:
+ * @box: (out): A Box.
+ * @start: A Start of box.
+ * @end: A End of box.
+ *
+ * Initialize a box with @start and @end.
+ */
+void
+crank_box3_init (CrankBox3      *box,
+                 CrankVecFloat3 *start,
+                 CrankVecFloat3 *end)
+{
+  crank_vec_float3_copy (start, & box->start);
+  crank_vec_float3_copy (end,   & box->end);
+}
+
+/**
+ * crank_box3_init_uvec:
+ * @box: (out): A Box.
+ * @start_x: X Coord of start.
+ * @start_y: Y Coord of start.
+ * @start_z: Z Coord of start.
+ * @end_x: X Coord of end.
+ * @end_y: Y Coord of end.
+ * @end_z: Z Coord of end.
+ *
+ * Initialize a box with start and end.
+ */
+void
+crank_box3_init_uvec (CrankBox3    *box,
+                      const gfloat  start_x,
+                      const gfloat  start_y,
+                      const gfloat  start_z,
+                      const gfloat  end_x,
+                      const gfloat  end_y,
+                      const gfloat  end_z)
+{
+  box->start.x = start_x;
+  box->start.y = start_y;
+  box->start.z = start_z;
+  box->end.x = end_x;
+  box->end.y = end_y;
+  box->end.z = end_z;
+}
+
+/**
+ * crank_box3_init_range:
+ * @box: (out): A Box.
+ * @x: Range of x coord.
+ * @y: Range of y coord.
+ * @z: Range of z coord.
+ *
+ * Initialize a box with range of each coordination.
+ */
+void
+crank_box3_init_range (CrankBox3     *box,
+                       CrankRanFloat *x,
+                       CrankRanFloat *y,
+                       CrankRanFloat *z)
+{
+  box->start.x = x->start;
+  box->start.y = y->start;
+  box->start.z = z->start;
+  box->end.x = x->end;
+  box->end.y = y->end;
+  box->end.z = z->end;
+}
+
+/**
+ * crank_box3_init_unit:
+ * @box: (out): A Box.
+ *
+ * Initialize a unit box with 1x1 size.
+ */
+void
+crank_box3_init_unit (CrankBox3 *box)
+{
+  box->start.x = 0;
+  box->start.y = 0;
+  box->start.z = 0;
+  box->end.x = 1;
+  box->end.y = 1;
+  box->end.z = 1;
+}
+
+/**
+ * crank_box3_copy:
+ * @box: A Box.
+ * @other: (out): Another box.
+ *
+ * Copies a box.
+ */
+void
+crank_box3_copy (CrankBox3 *box,
+                 CrankBox3 *other)
+{
+  crank_vec_float3_copy (& box->start, & other->start);
+  crank_vec_float3_copy (& box->end, & other->end);
+}
+
+/**
+ * crank_box3_dup:
+ * @box: A Box.
+ *
+ * Duplicates a boxed type.
+ *
+ * Returns: (transfer full): Duplicated box.
+ */
+CrankBox3*
+crank_box3_dup (CrankBox3 *box)
+{
+  CrankBox3 *result = g_new (CrankBox3, 1);
+  crank_box3_copy (box, result);
+  return result;
+}
+
+/**
+ * crank_box3_has_inf:
+ * @box: A Box.
+ *
+ * Checks whether box has infinite.
+ *
+ * Returns: Whether box has infinite.
+ */
+gboolean
+crank_box3_has_inf (CrankBox3 *box)
+{
+  return isinff (box->start.x) ||
+         isinff (box->start.y) ||
+         isinff (box->start.z) ||
+         isinff (box->end.x) ||
+         isinff (box->end.y) ||
+         isinff (box->end.z);
+}
+
+
+/**
+ * crank_box3_has_nan:
+ * @box: A Box.
+ *
+ * Checks whether box has NaN.
+ *
+ * Returns: Whether box has NaN.
+ */
+gboolean
+crank_box3_has_nan (CrankBox3 *box)
+{
+  return isnanf (box->start.x) ||
+         isnanf (box->start.y) ||
+         isnanf (box->start.z) ||
+         isnanf (box->end.x) ||
+         isnanf (box->end.y) ||
+         isnanf (box->end.z);
+}
+
+
+
+/**
+ * crank_box3_get_x:
+ * @box: A Box.
+ * @x: (out): Range of x coord.
+ *
+ * Gets range of x of box.
+ */
+void
+crank_box3_get_x (CrankBox3     *box,
+                  CrankRanFloat *x)
+{
+  x->start = box->start.x;
+  x->end = box->end.x;
+}
+
+/**
+ * crank_box3_get_y:
+ * @box: A Box.
+ * @y: (out): Range of y coord.
+ *
+ * Gets range of y of box.
+ */
+void
+crank_box3_get_y (CrankBox3     *box,
+                  CrankRanFloat *y)
+{
+  y->start = box->start.y;
+  y->end = box->end.y;
+}
+
+/**
+ * crank_box3_get_z:
+ * @box: A Box.
+ * @z: (out): Range of z coord.
+ *
+ * Gets range of z of box.
+ */
+void
+crank_box3_get_z (CrankBox3     *box,
+                  CrankRanFloat *z)
+{
+  z->start = box->start.z;
+  z->end = box->end.z;
+}
+
+/**
+ * crank_box3_get_dimen:
+ * @box: A Box.
+ * @dimen: (out):
+ *
+ * Gets dimension of @box. This is analogy to crank_ran_float_get_length()
+ */
+void
+crank_box3_get_dimen (CrankBox3      *box,
+                      CrankVecFloat3 *dimen)
+{
+  crank_vec_float3_sub (& box->end, & box->start, dimen);
+}
+
+/**
+ * crank_box3_get_diaglen:
+ * @box: A Box.
+ *
+ * Gets length of diagonal line.
+ *
+ * Returns: Length of diagonal line.
+ */
+gfloat
+crank_box3_get_diaglen (CrankBox3 *box)
+{
+  CrankVecFloat3 dimen;
+  crank_box3_get_dimen (box, &dimen);
+  return crank_vec_float3_get_magn (&dimen);
+}
+
+
+
+/**
+ * crank_box3_contains:
+ * @box: A Box.
+ * @value: Value to check.
+ *
+ * Checks @value is in @box.
+ *
+ * Returns: Whether @value is in @box.
+ */
+gboolean
+crank_box3_contains (CrankBox3      *box,
+                     CrankVecFloat3 *value)
+{
+  return (box->start.x < value->x) && (value->x < box->end.x) &&
+         (box->start.y < value->y) && (value->y < box->end.y) &&
+         (box->start.z < value->z) && (value->z < box->end.z);
+}
+
+/**
+ * crank_box3_get:
+ * @box: A Box.
+ * @index: A Index.
+ * @res: (out): A Result value.
+ *
+ * Gets value of @box at @index. As box is 2 dimensional, @index is also vector.
+ * Index {0, 0} for @box->start, index {1, 1} for @box->end.
+ *
+ * If @index is out of a box [0, 1] x [0, 1], the value also out of @box.
+ */
+void
+crank_box3_get (CrankBox3      *box,
+                CrankVecFloat3 *index,
+                CrankVecFloat3 *res)
+{
+  res->x = (box->end.x * index->x) + (box->start.x * (1 - index->x));
+  res->y = (box->end.y * index->y) + (box->start.y * (1 - index->y));
+  res->z = (box->end.z * index->z) + (box->start.z * (1 - index->z));
+}
+
+/**
+ * crank_box3_index_of:
+ * @box: A Box.
+ * @value: A Value to get index.
+ * @index: (out): A Index.
+ *
+ * Gets index of @value. As value is 2 dimensional, @index is alos vector.
+ *
+ * If @value is out of @box, @index also out of a box [0, 1] x [0, 1].
+ */
+void
+crank_box3_index_of (CrankBox3      *box,
+                     CrankVecFloat3 *value,
+                     CrankVecFloat3 *index)
+{
+  CrankVecFloat3 dimen;
+  CrankVecFloat3 mval;
+
+  crank_box3_get_dimen (box, &dimen);
+  crank_vec_float3_sub (value, & box->start, & mval);
+  crank_vec_float3_cmpdiv (& mval, & dimen, index);
+}
+
+/**
+ * crank_box3_clamp:
+ * @box: A Box.
+ * @value: A Value to clamp.
+ * @res: (out): A Clamped value.
+ *
+ * Clamp a value in @box by component wise.
+ *
+ * Clamped value is on edge or vertex, at least.
+ */
+void
+crank_box3_clamp (CrankBox3      *box,
+                  CrankVecFloat3 *value,
+                  CrankVecFloat3 *res)
+{
+  res->x = MAX(box->start.x, value->x);
+  res->x = MIN(box->end.x, res->x);
+
+  res->y = MAX(box->start.y, value->y);
+  res->y = MIN(box->end.y, res->y);
+
+  res->z = MAX(box->start.z, value->z);
+  res->z = MIN(box->end.z, res->z);
+}
+
+/**
+ * crank_box3_get_intersection:
+ * @box: A Box.
+ * @other: A other box.
+ * @intersect: (out): Intersection.
+ *
+ * Get intersecion of two items. If two does not intersect, %FALSE will be
+ * returned, and @intersect will be set to [0, 0] x [0, 0]
+ *
+ * Returns: Whether two boxes has intersection, and @intersect has been set.
+ */
+gboolean
+crank_box3_get_intersection (CrankBox3 *box,
+                             CrankBox3 *other,
+                             CrankBox3 *intersect)
+{
+  CrankRanFloat box_x;
+  CrankRanFloat other_x;
+
+  CrankRanFloat box_y;
+  CrankRanFloat other_y;
+
+  CrankRanFloat box_z;
+  CrankRanFloat other_z;
+
+  CrankRanFloat ix;
+  CrankRanFloat iy;
+  CrankRanFloat iz;
+
+  gboolean      result;
+
+  crank_box3_get_x (box, &box_x);
+  crank_box3_get_x (other, &other_x);
+  crank_box3_get_y (box, &box_y);
+  crank_box3_get_y (other, &other_y);
+  crank_box3_get_z (box, &box_z);
+  crank_box3_get_z (other, &other_z);
+
+  result = crank_ran_float_intersection (&box_x, &other_x, &ix) &&
+           crank_ran_float_intersection (&box_y, &other_y, &iy) &&
+           crank_ran_float_intersection (&box_z, &other_z, &iz);
+
+  if (intersect != NULL)
+    crank_box3_init_range (intersect, &ix, &iy, &iz);
 
   return result;
 }
--- crankshape/crankbox.h
+++ crankshape/crankbox.h
@@ -35,8 +35,14 @@ G_BEGIN_DECLS
 
 typedef struct _CrankBox2 CrankBox2;
 
+typedef struct _CrankBox3 CrankBox3;
+
+
 #define CRANK_TYPE_BOX2 crank_box2_get_type ()
 GType           crank_box2_get_type (void);
+
+#define CRANK_TYPE_BOX3 crank_box3_get_type ()
+GType           crank_box3_get_type (void);
 
 /**
  * CrankBox2:
@@ -48,6 +54,19 @@ GType           crank_box2_get_type (void);
 struct _CrankBox2 {
   CrankVecFloat2        start;
   CrankVecFloat2        end;
+};
+
+
+/**
+ * CrankBox3:
+ * @start: Start of box.
+ * @end: End of box.
+ *
+ * Represents a box shaped range between two #CrankVecFloat3.
+ */
+struct _CrankBox3 {
+  CrankVecFloat3        start;
+  CrankVecFloat3        end;
 };
 
 
@@ -114,6 +133,77 @@ void            crank_box2_clamp       (CrankBox2      *box,
 gboolean        crank_box2_get_intersection (CrankBox2 *box,
                                              CrankBox2 *other,
                                              CrankBox2 *intersect);
+
+
+
+
+void            crank_box3_init        (CrankBox3      *box,
+                                        CrankVecFloat3 *start,
+                                        CrankVecFloat3 *end);
+
+void            crank_box3_init_uvec   (CrankBox3      *box,
+                                        const gfloat    start_x,
+                                        const gfloat    start_y,
+                                        const gfloat    start_z,
+                                        const gfloat    end_x,
+                                        const gfloat    end_y,
+                                        const gfloat    end_z);
+
+void            crank_box3_init_range  (CrankBox3      *box,
+                                        CrankRanFloat  *x,
+                                        CrankRanFloat  *y,
+                                        CrankRanFloat  *z);
+
+void            crank_box3_init_unit   (CrankBox3      *box);
+
+void            crank_box3_copy        (CrankBox3      *box,
+                                        CrankBox3      *other);
+
+CrankBox3      *crank_box3_dup         (CrankBox3      *box);
+
+
+gboolean        crank_box3_has_inf     (CrankBox3      *box);
+
+gboolean        crank_box3_has_nan     (CrankBox3      *box);
+
+
+
+void            crank_box3_get_x       (CrankBox3      *box,
+                                        CrankRanFloat  *x);
+
+void            crank_box3_get_y       (CrankBox3      *box,
+                                        CrankRanFloat  *y);
+
+void            crank_box3_get_z       (CrankBox3      *box,
+                                        CrankRanFloat  *z);
+
+void            crank_box3_get_dimen   (CrankBox3      *box,
+                                        CrankVecFloat3 *dimen);
+
+gfloat          crank_box3_get_diaglen (CrankBox3      *box);
+
+
+
+
+
+gboolean        crank_box3_contains    (CrankBox3      *box,
+                                        CrankVecFloat3 *value);
+
+void            crank_box3_get         (CrankBox3      *box,
+                                        CrankVecFloat3 *index,
+                                        CrankVecFloat3 *res);
+
+void            crank_box3_index_of    (CrankBox3      *box,
+                                        CrankVecFloat3 *value,
+                                        CrankVecFloat3 *index);
+
+void            crank_box3_clamp       (CrankBox3      *box,
+                                        CrankVecFloat3 *value,
+                                        CrankVecFloat3 *res);
+
+gboolean        crank_box3_get_intersection (CrankBox3 *box,
+                                             CrankBox3 *other,
+                                             CrankBox3 *intersect);
 
 G_END_DECLS
 
