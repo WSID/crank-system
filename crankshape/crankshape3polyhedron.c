313893296cce25768964bb28e11602e8238c187c
WSID <jongsome@gmail.com>
2015-12-01 11:49:12 +0900

    CrankShape3Polyhedron: Inherits from CrankShape3Vertexed.

--- crankshape/crankshape3polyhedron.c
+++ crankshape/crankshape3polyhedron.c
@@ -44,50 +44,21 @@
 
 //////// List of virtual functions /////////////////////////////////////////////
 
-static void  crank_shape3_polyhedron_get_property (GObject    *object,
-                                                    guint       prop_id,
-                                                    GValue     *value,
-                                                    GParamSpec *pspec);
-
-static CrankShape3Polyhedron *crank_shape3_polyhedron_approximate_polyhedron (CrankShape3Finite *shape);
-
+static guint crank_shape3_polyhedron_get_dimension (CrankShape3Vertexed *shape);
 
 static void crank_shape3_polyhedron_def_get_face_normal (CrankShape3Polyhedron *shape,
                                                           const guint             fid,
                                                           CrankVecFloat3         *nor);
 
-static CrankShape2Polygon* crank_shape3_polyhedron_def_get_face_as_shape (CrankShape3Polyhedron *shape,
-                                                                           const guint             fid,
-                                                                           CrankTrans3            *pos);
-
-
-static guint crank_shape3_polyhedron_def_get_farthest_vertex (CrankShape3Polyhedron *shape,
-                                                              CrankVecFloat3         *dir);
-
 static guint crank_shape3_polyhedron_def_get_normal_face (CrankShape3Polyhedron *shape,
                                                           CrankVecFloat3         *nor);
-
-
-
-//////// Prioerty and signals //////////////////////////////////////////////////
-
-enum {
-  PROP_0,
-  PROP_NVERTICES,
-  PROP_NEDGES,
-  PROP_NFACES,
-
-  PROP_COUNTS
-};
-
-static GParamSpec *pspecs[PROP_COUNTS] = {NULL};
 
 
 //////// Type definition ///////////////////////////////////////////////////////
 
 G_DEFINE_ABSTRACT_TYPE (CrankShape3Polyhedron,
                         crank_shape3_polyhedron,
-                        CRANK_TYPE_SHAPE3_FINITE)
+                        CRANK_TYPE_SHAPE3_VERTEXED)
 
 
 //////// GTypeInstance /////////////////////////////////////////////////////////
@@ -101,77 +72,23 @@ crank_shape3_polyhedron_init (CrankShape3Polyhedron *self)
 void
 crank_shape3_polyhedron_class_init (CrankShape3PolyhedronClass *c)
 {
-  GObjectClass *c_gobject;
-  CrankShape3FiniteClass *c_shape3finite;
+  CrankShape3VertexedClass *c_shape3vertexed;
 
-  c_gobject = G_OBJECT_CLASS (c);
+  c_shape3vertexed = CRANK_SHAPE3_VERTEXED_CLASS (c);
 
-  c_gobject->get_property = crank_shape3_polyhedron_get_property;
-
-  pspecs[PROP_NVERTICES] = g_param_spec_uint ("nvertices", "nvertices",
-                                              "Number of vertices",
-                                              1, G_MAXUINT, 4,
-                                              G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
-
-  pspecs[PROP_NEDGES] = g_param_spec_uint ("nedges", "nedges",
-                                           "Number of edges",
-                                           0, G_MAXUINT, 4,
-                                           G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
-
-  pspecs[PROP_NFACES] = g_param_spec_uint ("nfaces", "nfaces",
-                                           "Number of faces",
-                                           0, G_MAXUINT, 4,
-                                           G_PARAM_READABLE | G_PARAM_STATIC_STRINGS);
-
-  g_object_class_install_properties (c_gobject, PROP_COUNTS, pspecs);
-
-
-
-  c_shape3finite = CRANK_SHAPE3_FINITE_CLASS (c);
-
-  c_shape3finite->approximate_polyhedron = crank_shape3_polyhedron_approximate_polyhedron;
-
+  c_shape3vertexed->get_dimension = crank_shape3_polyhedron_get_dimension;
 
   c->get_face_normal = crank_shape3_polyhedron_def_get_face_normal;
-  c->get_face_as_shape = crank_shape3_polyhedron_def_get_face_as_shape;
-  c->get_farthest_vertex = crank_shape3_polyhedron_def_get_farthest_vertex;
   c->get_normal_face = crank_shape3_polyhedron_def_get_normal_face;
 }
 
-//////// GObject ///////////////////////////////////////////////////////////////
+//////// CrankShape3Vertexed ///////////////////////////////////////////////////
 
-static void
-crank_shape3_polyhedron_get_property (GObject    *object,
-                                       guint       prop_id,
-                                       GValue     *value,
-                                       GParamSpec *pspec)
+static guint
+crank_shape3_polyhedron_get_dimension (CrankShape3Vertexed *shape)
 {
-  CrankShape3Polyhedron *self = CRANK_SHAPE3_POLYHEDRON (object);
-  switch (prop_id)
-    {
-    case PROP_NVERTICES:
-      g_value_set_uint (value, crank_shape3_polyhedron_get_nvertices (self));
-      break;
-    case PROP_NEDGES:
-      g_value_set_uint (value, crank_shape3_polyhedron_get_nedges (self));
-      break;
-    case PROP_NFACES:
-      g_value_set_uint (value, crank_shape3_polyhedron_get_nfaces (self));
-      break;
-
-    default:
-      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
-    }
+  return 3;
 }
-
-//////// CrankShape3Finite /////////////////////////////////////////////////////
-
-static CrankShape3Polyhedron*
-crank_shape3_polyhedron_approximate_polyhedron (CrankShape3Finite *shape)
-{
-  return CRANK_SHAPE3_POLYHEDRON (g_object_ref (shape));
-}
-
 
 //////// Default Implementations ///////////////////////////////////////////////
 
@@ -180,6 +97,8 @@ crank_shape3_polyhedron_def_get_face_normal (CrankShape3Polyhedron *shape,
                                               const guint             fid,
                                               CrankVecFloat3         *nor)
 {
+  CrankShape3Vertexed *vertexed;
+
   guint *vids;
   guint nvids;
 
@@ -189,14 +108,16 @@ crank_shape3_polyhedron_def_get_face_normal (CrankShape3Polyhedron *shape,
   CrankVecFloat3 seg_a;
   CrankVecFloat3 seg_b;
 
-  vids = crank_shape3_polyhedron_get_face_vertices (shape, fid, &nvids);
+  vertexed = (CrankShape3Vertexed*)shape;
 
-  crank_shape3_polyhedron_get_vertex_pos (shape, vids[0], &vert_a);
-  crank_shape3_polyhedron_get_vertex_pos (shape, vids[1], &vert_b);
+  vids = crank_shape3_vertexed_get_face_vertices (vertexed, fid, &nvids);
+
+  crank_shape3_vertexed_get_vertex_pos (vertexed, vids[0], &vert_a);
+  crank_shape3_vertexed_get_vertex_pos (vertexed, vids[1], &vert_b);
 
   crank_vec_float3_sub (&vert_b, &vert_a, &seg_a);
 
-  crank_shape3_polyhedron_get_vertex_pos (shape, vids[2], &vert_a);
+  crank_shape3_vertexed_get_vertex_pos (vertexed, vids[2], &vert_a);
 
   crank_vec_float3_sub (&vert_a, &vert_b, &seg_b);
 
@@ -204,51 +125,12 @@ crank_shape3_polyhedron_def_get_face_normal (CrankShape3Polyhedron *shape,
   crank_vec_float3_unit_self (nor);
 }
 
-static CrankShape2Polygon*
-crank_shape3_polyhedron_def_get_face_as_shape (CrankShape3Polyhedron *shape,
-                                                const guint             fid,
-                                                CrankTrans3            *pos)
-{
-  return NULL;
-}
-
-
-static guint
-crank_shape3_polyhedron_def_get_farthest_vertex (CrankShape3Polyhedron *shape,
-                                                  CrankVecFloat3         *dir)
-{
-  guint i;
-  guint n;
-
-  guint r;
-  gfloat dist;
-  CrankVecFloat3 vertpos;
-
-  r = 0;
-  crank_shape3_polyhedron_get_vertex_pos (shape, 0, &vertpos);
-  dist = crank_vec_float3_dot (&vertpos, dir);
-
-  n = crank_shape3_polyhedron_get_nvertices (shape);
-  for (i = 1; i < n; i ++)
-    {
-      gfloat ndist;
-
-      crank_shape3_polyhedron_get_vertex_pos (shape, 0, &vertpos);
-      ndist = crank_vec_float3_dot (&vertpos, dir);
-
-      if (dist < ndist)
-        {
-          r = i;
-          dist = ndist;
-        }
-    }
-  return r;
-}
-
 static guint
 crank_shape3_polyhedron_def_get_normal_face (CrankShape3Polyhedron *shape,
                                               CrankVecFloat3         *nor)
 {
+  CrankShape3Vertexed *vertexed;
+
   guint vid;
   guint *fids;
   guint nfids;
@@ -259,8 +141,10 @@ crank_shape3_polyhedron_def_get_normal_face (CrankShape3Polyhedron *shape,
   gfloat dot;
   CrankVecFloat3 facenor;
 
-  vid = crank_shape3_polyhedron_get_farthest_vertex (shape, nor);
-  fids = crank_shape3_polyhedron_get_vertex_faces (shape, vid, &nfids);
+  vertexed = (CrankShape3Vertexed*)shape;
+
+  vid = crank_shape3_vertexed_get_farthest_vertex (vertexed, nor);
+  fids = crank_shape3_vertexed_get_vertex_faces (vertexed, vid, &nfids);
 
   r = fids[0];
   crank_shape3_polyhedron_get_face_normal (shape, r, &facenor);
@@ -284,196 +168,7 @@ crank_shape3_polyhedron_def_get_normal_face (CrankShape3Polyhedron *shape,
 
 
 
-
-
-//////// Shape Properties //////////////////////////////////////////////////////
-
-/**
- * crank_shape3_polyhedron_get_nvertices:
- * @shape: A Shape.
- *
- * Gets number of vertices.
- *
- * Returns: Number of vertices in the shape.
- */
-guint
-crank_shape3_polyhedron_get_nvertices (CrankShape3Polyhedron *shape)
-{
-  CrankShape3PolyhedronClass *c = CRANK_SHAPE3_POLYHEDRON_GET_CLASS (shape);
-
-  return c->get_nvertices (shape);
-}
-
-/**
- * crank_shape3_polyhedron_get_nedges:
- * @shape: A Shape.
- *
- * Gets number of edges.
- *
- * Returns: Number of edges in the shape.
- */
-guint
-crank_shape3_polyhedron_get_nedges (CrankShape3Polyhedron *shape)
-{
-  CrankShape3PolyhedronClass *c = CRANK_SHAPE3_POLYHEDRON_GET_CLASS (shape);
-
-  return c->get_nedges (shape);
-}
-
-/**
- * crank_shape3_polyhedron_get_nfaces:
- * @shape: A Shape.
- *
- * Gets number of vertices.
- *
- * Returns: Number of faces in the shape.
- */
-guint
-crank_shape3_polyhedron_get_nfaces (CrankShape3Polyhedron *shape)
-{
-  CrankShape3PolyhedronClass *c = CRANK_SHAPE3_POLYHEDRON_GET_CLASS (shape);
-
-  return c->get_nfaces (shape);
-}
-
-
-
-//////// Vertex Properties /////////////////////////////////////////////////////
-
-/**
- * crank_shape3_polyhedron_get_vertex_pos:
- * @shape: A Shape.
- * @vid: Vertex ID
- * @pos: (out): Position of vertex.
- *
- * Gets position of a vertex.
- */
-void
-crank_shape3_polyhedron_get_vertex_pos (CrankShape3Polyhedron *shape,
-                                         const guint             vid,
-                                         CrankVecFloat3         *pos)
-{
-  CrankShape3PolyhedronClass *c = CRANK_SHAPE3_POLYHEDRON_GET_CLASS (shape);
-
-  c->get_vertex_pos (shape, vid, pos);
-}
-
-/**
- * crank_shape3_polyhedron_get_vertex_edges:
- * @shape: A Shape.
- * @vid: Vertex ID
- * @neids: (out): Length of edge array.
- *
- * Gets array of edges that shares vertex @vid.
- *
- * Returns: (array length=neids) (transfer full): Edges that shares a vertex.
- */
-guint*
-crank_shape3_polyhederon_get_vertex_edges (CrankShape3Polyhedron *shape,
-                                           const guint             vid,
-                                           guint                  *neids)
-{
-  CrankShape3PolyhedronClass *c = CRANK_SHAPE3_POLYHEDRON_GET_CLASS (shape);
-
-  return c->get_vertex_edges (shape, vid, neids);
-}
-
-/**
- * crank_shape3_polyhedron_get_vertex_faces:
- * @shape: A Shape.
- * @vid: Vertex ID
- * @nfids: (out): Length of face array.
- *
- * Gets array of faces that shares vertex @vid.
- *
- * Returns: (array length=nfids) (transfer full): Edges that shares a vertex.
- */
-guint*
-crank_shape3_polyhedron_get_vertex_faces (CrankShape3Polyhedron *shape,
-                                           const guint             vid,
-                                           guint                  *nfids)
-{
-  CrankShape3PolyhedronClass *c = CRANK_SHAPE3_POLYHEDRON_GET_CLASS (shape);
-
-  return c->get_vertex_faces (shape, vid, nfids);
-}
-
-
-/**
- * crank_shape3_polyhedreion_get_edge_vertices:
- * @shape: A Shape.
- * @eid: Edge ID
- * @vids: (out caller-allocates) (array fixed-size=2): Vertex IDs.
- *
- * Gets vertices of an edge.
- */
-void
-crank_shape3_polyhedron_get_edge_vertices (CrankShape3Polyhedron *shape,
-                                            const guint             eid,
-                                            guint                  *vids)
-{
-  CrankShape3PolyhedronClass *c = CRANK_SHAPE3_POLYHEDRON_GET_CLASS (shape);
-
-  return c->get_edge_vertices (shape, eid, vids);
-}
-
-/**
- * crank_shape3_polyhedreion_get_edge_faces:
- * @shape: A Shape.
- * @eid: Edge ID
- * @fids: (out caller-allocates) (array fixed-size=2): Face IDs.
- *
- * Gets faces that share an edge.
- */
-void
-crank_shape3_polyhedron_get_edge_faces (CrankShape3Polyhedron *shape,
-                                            const guint             eid,
-                                            guint                  *fids)
-{
-  CrankShape3PolyhedronClass *c = CRANK_SHAPE3_POLYHEDRON_GET_CLASS (shape);
-
-  return c->get_edge_faces (shape, eid, fids);
-}
-
-/**
- * crank_shape3_polyhedron_get_face_vertices:
- * @shape: A Shape.
- * @fid: Face ID.
- * @nvids: (out): Length of vertices array.
- *
- * Gets vertices array that the face has.
- *
- * Returns: (array length=nvids) (transfer full): Array of vertices.
- */
-guint*
-crank_shape3_polyhedron_get_face_vertices (CrankShape3Polyhedron *shape,
-                                            const guint             fid,
-                                            guint                  *nvids)
-{
-  CrankShape3PolyhedronClass *c = CRANK_SHAPE3_POLYHEDRON_GET_CLASS (shape);
-
-  return c->get_face_vertices (shape, fid, nvids);
-}
-
-/**
- * crank_shape3_polyhedron_get_face_edges:
- * @shape: A Shape.
- * @fid: Face ID.
- * @neids: (out): Length of edges array.
- *
- * Gets edges array that the face has.
- *
- * Returns: (array length=neids) (transfer full): Array of edges.
- */
-guint*
-crank_shape3_polyhedron_get_face_edges (CrankShape3Polyhedron *shape,
-                                         const guint             fid,
-                                         guint                  *neids)
-{
-  CrankShape3PolyhedronClass *c = CRANK_SHAPE3_POLYHEDRON_GET_CLASS (shape);
-
-  return c->get_face_vertices (shape, fid, neids);
-}
+//////// Face properties ///////////////////////////////////////////////////////
 
 /**
  * crank_shape3_polyhedron_get_face_normal:
@@ -494,44 +189,7 @@ crank_shape3_polyhedron_get_face_normal (CrankShape3Polyhedron *shape,
 }
 
 
-
-/**
- * crank_shape3_polyhedron_get_face_as_shape:
- * @shape: A Shape.
- * @fid: Face ID.
- * @pos: (out): Position of face in @shape
- *
- * Gets a face as a #CrankShape2Polygon.
- *
- * Returns: (transfer full) (nullable): A Shape, or %NULL if not supported.
- */
-CrankShape2Polygon*
-crank_shape3_polyhedron_get_face_as_shape (CrankShape3Polyhedron *shape,
-                                            const guint             fid,
-                                            CrankTrans3            *pos)
-{
-  CrankShape3PolyhedronClass *c = CRANK_SHAPE3_POLYHEDRON_GET_CLASS (shape);
-
-  return c->get_face_as_shape (shape, fid, pos);
-}
-
-/**
- * crank_shape3_polyhedron_get_farthest_vertex:
- * @shape: A Shape
- * @dir: Direction
- *
- * Gets the farthest vertex in the polyhedron.
- *
- * Returns: A Vertex ID.
- */
-guint
-crank_shape3_polyhedron_get_farthest_vertex (CrankShape3Polyhedron *shape,
-                                              CrankVecFloat3         *dir)
-{
-  CrankShape3PolyhedronClass *c = CRANK_SHAPE3_POLYHEDRON_GET_CLASS (shape);
-
-  return c->get_farthest_vertex (shape, dir);
-}
+//////// Item picker ///////////////////////////////////////////////////////////
 
 /**
  * crank_shape3_polyhedron_get_normal_face:
--- crankshape/crankshape3polyhedron.h
+++ crankshape/crankshape3polyhedron.h
@@ -35,6 +35,7 @@
 #include "crankshape2.h"
 #include "crankshape3.h"
 #include "crankshape3finite.h"
+#include "crankshape3vertexed.h"
 
 G_BEGIN_DECLS
 
@@ -42,7 +43,7 @@ G_BEGIN_DECLS
 G_DECLARE_DERIVABLE_TYPE (CrankShape3Polyhedron,
                           crank_shape3_polyhedron,
                           CRANK, SHAPE3_POLYHEDRON,
-                          CrankShape3Finite)
+                          CrankShape3Vertexed)
 
 /**
  * CrankShape3PolyhedronVInfo:
@@ -80,162 +81,40 @@ typedef struct _CrankShape3PolyhedronFData {
 
 /**
  * CrankShape3PolyhedronClass:
- * @get_nvertices: Slot for crank_shape3_polyhedron_get_nvertices()
- * @get_nedges: Slot for crank_shape3_polyhedron_get_nedges()
- * @get_nfaces: Slot for crank_shape3_polyhedron_get_nfaces()
- * @get_vertex_pos: Slot for crank_shape3_polyhedron_get_vertex_pos()
- * @get_vertex_edges: Slot for crank_shape3_polyhedron_get_vertex_edges()
- * @get_vertex_faces: Slot for crank_shape3_polyhedron_get_vertex_faces()
- * @get_edge_vertices: Slot for crank_shape3_polyhedron_get_edge_vertices()
- * @get_edge_faces: Slot for crank_shape3_polyhedron_get_edge_faces()
- * @get_face_vertices: Slot for crank_shape3_polyhedron_get_face_vertices()
- * @get_face_edges: Slot for crank_shape3_polyhedron_get_face_edges()
  * @get_face_normal: Slot for crank_shape3_polyhedron_get_face_normal()
- * @get_face_as_shape: Slot for crank_shape3_polyhedron_get_face_as_shape()
- * @get_farthest_vertex: Slot for crank_shape3_polyhedron_get_farthest_vertex()
  * @get_normal_face: Slot for crank_shape3_polyhedron_get_normal_face()
  *
  * Virtual function table for #CrankShape3PolyhedronClass
  */
 struct _CrankShape3PolyhedronClass {
   /*< private >*/
-  CrankShape3FiniteClass _parent;
+  CrankShape3VertexedClass _parent;
 
   /*< public >*/
-  guint (*get_nvertices)    (CrankShape3Polyhedron *shape);
-  guint (*get_nedges)       (CrankShape3Polyhedron *shape);
-  guint (*get_nfaces)       (CrankShape3Polyhedron *shape);
-
-  /*< private >*/
-  gpointer _PADDING3;
-
-
-  /*< public >*/
-  void  (*get_vertex_pos)   (CrankShape3Polyhedron *shape,
-                             const guint             vid,
-                             CrankVecFloat3         *pos);
-
-  guint*(*get_vertex_edges) (CrankShape3Polyhedron *shape,
-                             const guint             vid,
-                             guint                  *neids);
-
-  guint*(*get_vertex_faces) (CrankShape3Polyhedron *shape,
-                             const guint             vid,
-                             guint                  *nfids);
-
-  /*< private >*/
-  gpointer _PADDING7;
-
-
-  /*< public >*/
-  void  (*get_edge_vertices)(CrankShape3Polyhedron *shape,
-                             const guint             eid,
-                             guint                  *vids);
-
-  void  (*get_edge_faces)   (CrankShape3Polyhedron *shape,
-                             const guint             eid,
-                             guint                  *fids);
-
-  /*< private >*/
-  gpointer _PADDING10;
-  gpointer _PADDING11;
-
-
-  /*< public >*/
-  guint*(*get_face_vertices)(CrankShape3Polyhedron *shape,
-                             const guint             fid,
-                             guint                  *nvids);
-
-  guint*(*get_face_edges)   (CrankShape3Polyhedron *shape,
-                             const guint             fid,
-                             guint                  *neids);
-
   void  (*get_face_normal)  (CrankShape3Polyhedron *shape,
                              const guint             fid,
                              CrankVecFloat3         *nor);
-
-  CrankShape2Polygon* (*get_face_as_shape) (CrankShape3Polyhedron *shape,
-                                            const guint             fid,
-                                            CrankTrans3            *pos);
-
-
-  guint (*get_farthest_vertex)(CrankShape3Polyhedron *shape,
-                               CrankVecFloat3         *dir);
 
   guint (*get_normal_face) (CrankShape3Polyhedron *shape,
                             CrankVecFloat3         *nor);
 
   /*< private >*/
-  gpointer _PADDING18;
-  gpointer _PADDING19;
+  gpointer _PADDING2;
+  gpointer _PADDING3;
 
 };
 
-
-//////// Shape Property ////////////////////////////////////////////////////////
-
-guint crank_shape3_polyhedron_get_nvertices                   (CrankShape3Polyhedron *shape);
-
-guint crank_shape3_polyhedron_get_nedges                      (CrankShape3Polyhedron *shape);
-
-guint crank_shape3_polyhedron_get_nfaces                      (CrankShape3Polyhedron *shape);
-
-//////// Vertex Property ///////////////////////////////////////////////////////
-
-void crank_shape3_polyhedron_get_vertex_pos                   (CrankShape3Polyhedron *shape,
-                                                                const guint             vid,
-                                                                CrankVecFloat3         *pos);
-
-guint *crank_shape3_polyhedron_get_vertex_edges               (CrankShape3Polyhedron *shape,
-                                                                const guint             vid,
-                                                                guint                  *neids);
-
-guint *crank_shape3_polyhedron_get_vertex_faces               (CrankShape3Polyhedron *shape,
-                                                                const guint             vid,
-                                                                guint                  *nfids);
-
-//////// Edge Property /////////////////////////////////////////////////////////
-
-void crank_shape3_polyhedron_get_edge_vertices                (CrankShape3Polyhedron *shape,
-                                                                const guint             eid,
-                                                                guint                  *vids);
-
-void crank_shape3_polyhedron_get_edge_faces                   (CrankShape3Polyhedron *shape,
-                                                                const guint             eid,
-                                                                guint                  *fids);
-
-//////// Face Property /////////////////////////////////////////////////////////
-
-guint *crank_shape3_polyhedron_get_face_vertices              (CrankShape3Polyhedron *shape,
-                                                                const guint             fid,
-                                                                guint                  *nvids);
-
-guint *crank_shape3_polyhedron_get_face_edges                 (CrankShape3Polyhedron *shape,
-                                                                const guint             fid,
-                                                                guint                  *neids);
+//////// Face Properties ///////////////////////////////////////////////////////
 
 void   crank_shape3_polyhedron_get_face_normal                (CrankShape3Polyhedron *shape,
-                                                                const guint             fid,
-                                                                CrankVecFloat3         *nor);
-
-CrankShape2Polygon *crank_shape3_polyhedron_get_face_as_shape (CrankShape3Polyhedron *shape,
-                                                                const guint             fid,
-                                                                CrankTrans3            *pos);
+                                                               const guint            fid,
+                                                               CrankVecFloat3        *nor);
 
 
 //////// Nearest item picker ///////////////////////////////////////////////////
 
-guint crank_shape3_polyhedron_get_farthest_vertex             (CrankShape3Polyhedron *shape,
-                                                                CrankVecFloat3         *dir);
-
 guint crank_shape3_polyhedron_get_normal_face                 (CrankShape3Polyhedron *shape,
-                                                                CrankVecFloat3         *nor);
-
-
-//////// Possible functions ////////////////////////////////////////////////////
-// CrankDigraph *crank_shape3_polyhedron_get_ve_digraph (CrankShape3Polyhedron *shape);
-//
-// void crank_shape3_polyhedron_assert_euler (CrankShape3Polyhedron);
+                                                               CrankVecFloat3        *nor);
 
 G_END_DECLS
 
