340474bd95a35d3429d83a213a81a81f2d4bd857
WSID <jongsome@gmail.com>
2016-02-12 19:32:07 +0900

    CrankCellSpace3: Added test case for CrankCellSpace3.

--- tests/c/base/test_cell_space.c
+++ tests/c/base/test_cell_space.c
@@ -31,6 +31,10 @@ static void test_2_new (void);
 static void test_2_resize (void);
 static void test_2_set (void);
 
+static void test_3_new (void);
+static void test_3_resize (void);
+static void test_3_set (void);
+
 
 //////// Main functions ////////////////////////////////////////////////////////
 
@@ -42,6 +46,10 @@ gint main (gint    argc,
   g_test_add_func ("/crank/base/cellspace/2/new",       test_2_new);
   g_test_add_func ("/crank/base/cellspace/2/resize",    test_2_resize);
   g_test_add_func ("/crank/base/cellspace/2/set",       test_2_set);
+
+  g_test_add_func ("/crank/base/cellspace/3/new",       test_3_new);
+  g_test_add_func ("/crank/base/cellspace/3/resize",    test_3_resize);
+  g_test_add_func ("/crank/base/cellspace/3/set",       test_3_set);
 
   return g_test_run();
 }
@@ -110,4 +118,79 @@ test_2_set (void)
   g_assert (crank_cell_space2_type_of (cs, 2, 2) == G_TYPE_FLOAT);
 
   crank_cell_space2_unref (cs);
+}
+
+static void
+test_3_new (void)
+{
+  guint i;
+  guint j;
+  guint k;
+
+  CrankCellSpace3 *cs = crank_cell_space3_new_with_size (4, 4, 4);
+
+  g_assert_cmpuint (crank_cell_space3_get_width (cs), ==, 4);
+  g_assert_cmpuint (crank_cell_space3_get_height (cs), ==, 4);
+  g_assert_cmpuint (crank_cell_space3_get_depth (cs), ==, 4);
+  for (i = 0; i < 4; i++)
+    {
+      for (j = 0; j < 4; j++)
+        {
+          for (k = 0; k < 4; k++)
+            {
+              g_assert (crank_cell_space3_is_unset (cs, i, j, k));
+            }
+        }
+    }
+
+  crank_cell_space3_set_int (cs, 2, 2, 2, 15);
+
+  crank_cell_space3_unref (cs);
+}
+
+static void
+test_3_resize (void)
+{
+  guint i;
+  guint j;
+  guint k;
+
+  CrankCellSpace3 *cs = crank_cell_space3_new ();
+  crank_cell_space3_set_width (cs, 4);
+  crank_cell_space3_set_height (cs, 4);
+  crank_cell_space3_set_depth (cs, 4);
+
+  g_assert_cmpuint (crank_cell_space3_get_width (cs), ==, 4);
+  g_assert_cmpuint (crank_cell_space3_get_height (cs), ==, 4);
+  g_assert_cmpuint (crank_cell_space3_get_depth (cs), ==, 4);
+  for (i = 0; i < 4; i++)
+    {
+      for (j = 0; j < 4; j++)
+        {
+          for (k = 0;k < 4; k++)
+            {
+              g_assert (crank_cell_space3_is_unset (cs, i, j, k));
+            }
+        }
+    }
+
+  crank_cell_space3_set_int (cs, 2, 2, 2, 15);
+
+  crank_cell_space3_unref (cs);
+}
+
+static void
+test_3_set (void)
+{
+  CrankCellSpace3 *cs = crank_cell_space3_new ();
+  crank_cell_space3_set_width (cs, 4);
+  crank_cell_space3_set_height (cs, 4);
+  crank_cell_space3_set_depth (cs, 4);
+
+  crank_cell_space3_set_int (cs, 2, 2, 2, 15);
+  crank_cell_space3_set_float (cs, 2, 2, 2, 4.5f);
+
+  g_assert (crank_cell_space3_type_of (cs, 2, 2, 2) == G_TYPE_FLOAT);
+
+  crank_cell_space3_unref (cs);
 }
